12a56933c65771ecfa6d022de2da9bf5a9c00a8d
Matheus <matheus_rossini2008@hotmail.com>
2024-06-25 15:23:10 -0300

    Projeto TypeScript

---
+++ src/controllers/userController.test.ts
@@ -0,0 +1,35 @@
+import { makeMockResponse } from "../mocks/mockResponse";
+import { UsersController } from "./usersController"
+import { Request } from "express";
+
+describe('Users Controller', () => {
+    const usersController = new UsersController();
+
+    const mockRequest = {} as Request;
+    const mockResponse = makeMockResponse()
+    it("Deve listar os nossos usuários", () => {
+        usersController.listarUsuario(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(200)
+        expect(mockResponse.state.json).toHaveLength(4)
+    })
+
+    it('Deve criar um novo usuário', () => {
+        mockRequest.body = {
+            name: 'Novo usuário'
+        }
+
+        usersController.criarUsuario(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(201)
+        expect(mockResponse.state.json).toMatchObject({'mensagem': `Usuário Novo usuário criado`})
+    })
+
+    it('Não deve criar um novo usuário sem nome', () => {
+        mockRequest.body = {
+            name: ''
+        }
+    
+        usersController.criarUsuario(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(403)
+        expect(mockResponse.state.json).toMatchObject({'mensagem': 'Não é possível criar usuários sem um nome'})
+    })
+})
